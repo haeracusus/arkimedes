277a5cc590b1fad53ff71b9556b0978889d0e0cf
haeracusus <118097924+haeracusus@users.noreply.github.com>
2023-01-05 11:23:15 +0800

    arkimedes
    da scrip

---
+++ arkimedes.py
@@ -0,0 +1,7 @@
+import os
+#xfce, xarchiver, yay, firefox, neovim, htop, neofetch [!!ABSOLUTELY NECESSARY FOR EVERY INSTALL!!], vlc, pipewire over fartaudio, gvfs, fastest mirrors
+os.system('sudo pacman -S reflector')
+os.system('sudo reflector --verbose --sort rate -l 15 -p https --save /etc/pacman.d/mirrorlist')
+os.system('sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd')
+os.system('sudo pacman -S xfce4 xfce4-goodies xarchiver unzip unrar firefox neovim gvfs pipewire pipewire-pulse pipewire-alsa pipewire-jack lightdm lightdm-gtk-greeter gvfs')
+os.system('systemctl enable lightdm')
