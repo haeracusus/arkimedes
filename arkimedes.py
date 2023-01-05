74a76d352e44b1261e4648ad817e14cf00e3383f
haeracusus <118097924+haeracusus@users.noreply.github.com>
2023-01-05 11:33:30 +0800

    arkimedes update 2
    wireplumber

--- arkimedes.py
+++ arkimedes.py
@@ -3,5 +3,5 @@ import os
 os.system('sudo pacman -S reflector')
 os.system('sudo reflector --verbose --sort rate -l 15 -p https --save /etc/pacman.d/mirrorlist')
 os.system('sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd')
-os.system('sudo pacman -S xfce4 xfce4-goodies xarchiver fish unzip unrar firefox neovim gvfs pipewire pipewire-pulse pipewire-alsa pipewire-jack lightdm lightdm-gtk-greeter gvfs')
+os.system('sudo pacman -S xfce4 xfce4-goodies xarchiver fish unzip unrar firefox neovim gvfs pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber lightdm lightdm-gtk-greeter gvfs')
 os.system('systemctl enable lightdm && chsh -s $(which fish)')
